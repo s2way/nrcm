037e0392b4af7e2f87b352c7b614d812c889c37f
Davi Gabriel da Silva <davi.gbr@gmail.com>
2014-06-18 17:46:17 -0300

    Added code coverage test to posttest.

---
+++ cc.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var minimalCoverage = 90;
+
+var cc = JSON.parse(fs.readFileSync('cc.json', "utf8"));
+if (cc) {
+    console.log('-> Code Coverage: ' + cc.coverage + ' <-');
+    if (cc.coverage > minimalCoverage) {
+        process.exit(0);
+    } else {
+        console.log('ERROR: Code Coverage below minimum of ' + minimalCoverage + '%!');
+    }
+}
+
+process.exit(1);
+
