1656f952096e6f923501a5f95eb9351f4021f562
= <davi.gbr@gmail.com>
2014-09-16 10:20:52 -0300

    Validator and SchemaMatcher merged

--- src/Component/Builtin/Validator.js
+++ src/Component/Builtin/Validator.js
@@ -1,17 +1,21 @@
 /*jslint devel: true, node: true, indent: 4, vars: true, stupid: true, nomen: true */
 'use strict';
+
+var exceptions = require('../../exceptions');
+
 /**
  * The validator object
  *
  * @constructor
  * @method Validator
- * @param {object} validate The object that contains the rules
- * @param {number} timeout The timeout for the validation exception
+ * @param {object} params Must contain the validation rules (validate property) and may contain the timeout (in millis)
  */
-function Validator(validate, timeout) {
-    this.timeout = timeout || 10000;
-    this.validate = validate;
+function Validator(params) {
+    params = params || {};
+    this.timeout = params.timeout || 10000;
+    this.validate = params.validate;
 }
+
 // Validate fields
 Validator.prototype._succeeded = function (validatedFields) {
     var key;
@@ -28,6 +32,7 @@ Validator.prototype._succeeded = function (validatedFields) {
     }
     return true;
 };
+
 // Find all fields to validate
 Validator.prototype._hasValidatedAllFields = function (validatedFields, validate) {
     var key;
@@ -49,6 +54,7 @@ Validator.prototype._hasValidatedAllFields = function (validatedFields, validate
     }
     return true;
 };
+
 // isValid
 Validator.prototype._isValid = function (data, validatedFields, validate, originalData) {
     var n;
@@ -105,5 +111,73 @@ Validator.prototype.isValid = function (data, callback) {
     };
     timeoutFunc();
 };
+
+
+Validator.prototype._matchAgainst = function (data, level, validate) {
+    var n, test;
+    if (level === undefined) {
+        level = 1;
+        validate = this.validate;
+    } else {
+        level += 1;
+    }
+    // check schema field presence
+    for (n in data) {
+        if (data.hasOwnProperty(n)) {
+            // schema for this field was not set, block
+            if (validate[n] === undefined) {
+                return { 'field' : n, 'level' : level, 'error' : 'denied' };
+            }
+            // validate set and it is an object: recursive
+            if (typeof validate[n] === 'object') {
+                test = this._matchAgainst(data[n], level, validate[n]);
+                if (test !== true) {
+                    return test;
+                }
+            }
+        }
+    }
+    // check for required fields
+    for (n in validate) {
+        if (validate.hasOwnProperty(n)) {
+            if (validate[n] === true && data[n] === undefined) {
+                // required field not present
+                return { 'field' : n, 'level' : level, 'error' : 'required' };
+            }
+        }
+    }
+    return true;
+};
+
+Validator.prototype._isJSONValid = function (jsonOb) {
+    var newJSON;
+    if (jsonOb === undefined || jsonOb === null) {
+        return false;
+    }
+    try {
+        newJSON = JSON.parse(JSON.stringify(jsonOb));
+    } catch (e) {
+        return false;
+    }
+    if (Object.getOwnPropertyNames(newJSON).length > 0) {
+        return newJSON;
+    }
+    return false;
+};
+
+/**
+ * Match the data against the validate object specified in the constructor
+ * If there are fields in the data that are not specified in the validate object, this method returns false
+ * @param {object} data The data to be matched
+ * @return {boolean}
+ */
+Validator.prototype.match = function (data) {
+    var newData = this._isJSONValid(data);
+    if (!newData) {
+        throw new exceptions.IllegalArgument('The data is invalid!');
+    }
+    return this._matchAgainst(data);
+};
+
 
 module.exports = Validator;
--- test/Component/Builtin/Validator.js
+++ test/Component/Builtin/Validator.js
@@ -1,10 +1,12 @@
-/*jslint devel: true, node: true, indent: 4 */
+/*jslint devel: true, node: true, indent: 4, unparam: true */
 /*globals describe, it */
 'use strict';
 var assert = require('assert');
+var expect = require('expect.js');
 var Validator = require('./../../../src/Component/Builtin/Validator');
 
 describe('Validator.js', function () {
+
     describe('isValid', function () {
         var logradouro = 'Rua José de Alencar';
         var email = 'davi@versul.com.br';
@@ -45,7 +47,7 @@ describe('Validator.js', function () {
                     callback(true);
                 }
             };
-            var validator = new Validator(validate, 100);
+            var validator = new Validator({ 'validate' : validate, 'timeout' : 100 });
             validator.isValid(data, function (expired, succeeded, validatedFields) {
                 var expectedValidatedFields = {
                     'cpf' : true,
@@ -88,7 +90,7 @@ describe('Validator.js', function () {
                     callback(true);
                 }
             };
-            var validator = new Validator(validate, 10);
+            var validator = new Validator({ 'validate': validate, 'timeout': 10 });
             validator.isValid(data, function (expired, succeeded, validatedFields) {
                 var expectedValidatedFields = {
                     'e-mail' : true,
@@ -102,6 +104,127 @@ describe('Validator.js', function () {
                 assert.equal(JSON.stringify(expectedValidatedFields), JSON.stringify(validatedFields));
                 done();
             });
+        });
+    });
+
+    describe('match', function () {
+        it('should throw an error if the data is invalid', function () {
+            var validate = {'title': false, 'description': false};
+            var validator;
+            try {
+                validator = new Validator({'validate' : validate});
+            } catch (e) {
+                assert.fail();
+            }
+            try {
+                validator.match({'title' : function () {
+                    return;
+                }});
+                assert.fail();
+            } catch (e) {
+                assert.equal('IllegalArgument', e.name);
+            }
+        });
+        it('should throw an error if the data is undefined or null', function () {
+            var validator = new Validator({'validate' : {}, 'field' : true });
+            try {
+                validator.match();
+                assert.fail();
+            } catch (e) {
+                assert.equal('IllegalArgument', e.name);
+            }
+        });
+        it('should throw an error if the data is other thing besides a json', function () {
+            var validator = new Validator({'validate' : {}, 'field' : true });
+            try {
+                validator.match(function () {
+                    return;
+                });
+                assert.fail();
+            } catch (e) {
+                expect(e.name).to.be('IllegalArgument');
+            }
+        });
+        it('should return true if the data is according to the validate', function () {
+            var validate = {
+                'string' : true,
+                'array' : true,
+                'object' : {
+                    'object' : {
+                        'array' : false
+                    },
+                    'number' : true
+                }
+            };
+            var data = {
+                'string': 'string',
+                'array' : [0, 1, 3],
+                'object' : {
+                    'object' : {'array' : [0, 1, 2]},
+                    'number' : 100
+                }
+            };
+            var validator;
+            try {
+                validator = new Validator({'validate' : validate});
+            } catch (e) {
+                assert.fail();
+            }
+            assert(validator.match(data));
+        });
+        it('should return an error if there is a missing required field', function () {
+            var validate = {
+                'string' : false,
+                'array' : true,
+                'object' : {
+                    'object' : {
+                        'array' : false
+                    },
+                    'number' : false
+                }
+            };
+            var data = {
+                'string': 'string',
+                'object' : {
+                    'object' : {'array' : [0, 1, 2]},
+                    'number' : 'not_number'
+                }
+            };
+            var validator = new Validator({'validate' : validate});
+            var result = validator.match(data);
+            assert.equal(
+                JSON.stringify({
+                    'field' : 'array',
+                    'level' : 1,
+                    'error' : 'required'
+                }),
+                JSON.stringify(result)
+            );
+        });
+        it('should return an error if there is a field that is not specified in the validate', function () {
+            var validate = {
+                'string' : false,
+                'array' : false,
+                'object' : {
+                    'object' : false,
+                    'number' : false
+                }
+            };
+            var data = {
+                'object' : {
+                    'iShouldnt' : 'beHere'
+                }
+            };
+            var validator = new Validator({'validate' : validate});
+            var result = validator.match(data);
+            assert.equal(
+                JSON.stringify({
+                    'field' : 'iShouldnt',
+                    'level' : 2,
+                    'error' : 'denied'
+                }),
+                JSON.stringify(result)
+            );
         });
     });
 
